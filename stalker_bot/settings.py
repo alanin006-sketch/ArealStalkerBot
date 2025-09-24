236d19de40d363937014562ebd944f70788fc66f
alanin <alanin006@gmail.com>
2025-09-24 08:10:18 +0300

    Update settings.py

--- stalker_bot/settings.py
+++ stalker_bot/settings.py
@@ -67,8 +67,8 @@ TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
